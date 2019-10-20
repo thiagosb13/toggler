77c782822ad69253d1ac49c96577761122fc45f5
Thiago Silva <thiagosb13@gmail.com>
2019-10-20 17:54:14 -0300

    Creating project

---
+++ src/main/java/com/thiagobezerra/toggler/TogglerApplication.java
@@ -0,0 +1,13 @@
+package com.thiagobezerra.toggler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogglerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogglerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagobezerra/toggler/TogglerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagobezerra.toggler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogglerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
