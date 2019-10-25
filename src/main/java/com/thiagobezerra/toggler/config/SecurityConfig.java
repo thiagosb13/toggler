7d6eaa05768e17f9c748029f86a43699fa85c3c5
Thiago Silva <thiagosb13@gmail.com>
2019-10-24 22:25:46 -0300

    Configuring basic authentication on API

---
+++ src/main/java/com/thiagobezerra/toggler/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.thiagobezerra.toggler.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests().anyRequest().authenticated()
+            .and()
+            .httpBasic();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("admin")
+            .password("$2a$10$wteAIyMp/S2VgN71Xf9mqe5UHIq35y1ZbR1KuCbRAAoQWrlJ7CGEG")
+            .roles("USER");
+    }
+}
